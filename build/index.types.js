3a69b5859d33cd296d22edf8cae7e9f55f0867c9
Stuart Grant <grantsru@gmail.com>
2019-09-19 21:43:17 -0400

    Remove build/main JS

--- build/index.types.js
+++
@@ -1,9 +0,0 @@
-// export type BankAccount = {
-//   money: number;
-//   deposit: (value: number) => void;
-// };
-// export type Person = {
-//   name: string;
-//   bankAccount: BankAccount;
-//   hobbies: string[];
-// };
